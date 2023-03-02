06eb41e22250df982022b2efc6da14026011597e
Paula Adolfi <paula.adolfi@gmail.com>
2023-03-02 11:30:25 -0300

    Actualizacion about-me

--- src/pages/SobreMi.js
+++ src/pages/SobreMi.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SobreMi = (props) => {
 	return (
@@ -12,12 +13,17 @@ const SobreMi = (props) => {
 					</div>
 				</div>
 				<p>Me encuentro cursando el segundo año de la tecnicatura superior en desarrollo de software y dando mis primeros pasos como desarrolladora. El descubrimiento del mundo IT me tiene fascinada, por lo que además de los conocimientos académicos estoy en constante capacitación.</p>
-				<p>Recientemente obtuve mi certificación del curso <a href="https://drive.google.com/file/d/1fGSkFsIXGC2hQ4LBqC7dRAGkpe74GitK/view?usp=sharing" target="_blank">Experto Universitario en Programación Web Full Stack de la UTN</a>.</p>
-				<p>Actualmente también soy parte del programa ORACLE NEXT EDUCATION, brindado por Oracle y Alura Latam para seguir formándome como full stack y aprendiendo a manejar nuevos lenguajes para mi, como Java. Ya completé dos rutas técnicas completas: <a href="https://res.cloudinary.com/dz6gqdulr/image/upload/v1662726309/portfolio/desarrollo_personal_ahio3u.jpg">Desarrollo Personal</a> y <a href="https://res.cloudinary.com/dz6gqdulr/image/upload/v1662726379/portfolio/ruta_principiante_1_lsstsq.jpg"target="_blank"> Principiante en Programación</a>.</p>
-				<p>Además, me encuentro capacitándome de manera autodidacta a través de distintas herramientas gratuitas, como por ejemplo en Free Code Camp, donde obtuve la certificación en <a href="https://freecodecamp.org/certification/fccf2c85b23-e44d-4f8a-acf8-416d5d9bdd06/responsive-web-design" target="_blank">Responsive Web Design</a>.</p>
-				<p>Si bien no poseo aun experiencia en este área traigo conmigo un abanico de habilidades blandas que he ido adquiriendo en mis mas de 15 años de experiencia laboral. Espero con ansias mi primera oportunidad laboral para poner en práctica los conocimientos adquiridos, pero aun mucho mas de seguir aprendiendo.</p>
+				<p class="subrayado">Certificaciones:</p>
+				<ul> 
+					<li class="certificaciones"><a href="https://drive.google.com/file/d/1fGSkFsIXGC2hQ4LBqC7dRAGkpe74GitK/view?usp=sharing" target="_blank">Experto Universitario en Programación Web Full Stack de la UTN</a>, donde trabajamos con React en el frontend y nodeJs junto con una gran cantidad de librerias en el backend.</li>
+					<li class="certificaciones"><a href="https://drive.google.com/file/d/1x1j9jMzX22bkiG3LM1yc8DtJRyxKAjAN/view?usp=sharing" target="_blank">Programa ORACLE NEXT EDUCATION, brindado por Oracle y Alura Latam</a>, que incluye 3 rutas principales: <a href="https://drive.google.com/file/d/1Hbo70Hi_u2wwamwwRfLvTorcjbTk7SgU/view?usp=sharing"target="_blank"> Principiante en Programación</a>, <a href="https://drive.google.com/file/d/1ThyQtRU5wzhQMOjBmaVMf_oFt0jozhMT/view?usp=sharing"target="_blank"> Front end</a> y <a href="https://drive.google.com/file/d/1OYqlo6LrXmqhbdNApHd4GCgCqgSPXOES/view?usp=sharing"target="_blank"> Java orientado a objetos</a>.</li>
+					<li class="certificaciones"><a href="https://drive.google.com/file/d/1FOvHvnjdTJsPlEjGuml28--SQfo1-8pE/view?usp=sharing" target="_blank">Argentina Programa #YoProgramo</a>, otorgado por el INTI, enfocado en lógica de programación orientada a objetos utlizando lenguajes como javasScript y Ruby.</li>
+					<li class="certificaciones">Actualmente soy parte del programa Argentina Programa 4.0. Dentro de dicha formación ya obtuve mi certificación en <a href="https://drive.google.com/file/d/1RmKzNDbwON5PFlCTsrhb17C6wc73m3ty/view?usp=sharing" target="_blank">Introducción a la Programación</a> y me encuentro cursando la etapa de backend con Java.</li>
+					<li class="certificaciones">Además, siempre continúo capacitándome de manera autodidacta a través de distintas herramientas gratuitas, como por ejemplo en Free Code Camp, donde obtuve la certificación en <a href="https://freecodecamp.org/certification/fccf2c85b23-e44d-4f8a-acf8-416d5d9bdd06/responsive-web-design" target="_blank">Responsive Web Design</a></li>
+				</ul>
+				<p>Si bien no poseo aun experiencia en este área traigo conmigo un abanico de habilidades blandas que he ido adquiriendo en mis más de 15 años de experiencia laboral. El trabajar tantos años en el sector postventa me hizo tener una gran capacidad para la resolución de conflictos, el análisis de posibilidades y prioridades, pero sobre todo en cuanto a las relaciones interpersonales; mientras que las tareas desarrolladas en el sector administrativo me hicieron una persona sumamente organizada, responsable y con profesionalidad en el manejo de la información.</p>
 				<div class="CV">
-					<p><a href="https://drive.google.com/file/d/1g2O2juTm2RPAQQVcWcFBcGUiLN_LF7Nq/view?usp=sharing" target="_blank">Descargar Curriculum Vitae </a></p>
+					<p><a href="https://drive.google.com/file/d/17ZtLnaj3kdJZYsDJ5oN5NrtLKxMSOoEx/view?usp=sharing" target="_blank">Descargar Curriculum Vitae </a></p>
 				</div>
 			</section>
 			<section id="Skills">
@@ -37,8 +43,10 @@ const SobreMi = (props) => {
 				<div class="img-skills">
 					<img class="border" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvWX1-RTRoxfuElIONw2WOa711Fp6bGUkRpR2_f7BuyfU5G_WPJQYH9sg2CRZesoSsT0Q&usqp=CAU" alt="nodeJs" />
 					<img class="border" src="https://res.cloudinary.com/dz6gqdulr/image/upload/v1662724566/portfolio/express_dcwdut.png" alt="express" />
+					<img class="border" src="https://cdn.icon-icons.com/icons2/2415/PNG/512/java_original_wordmark_logo_icon_146459.png" alt="java" />
 					<img class="border" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIxHxPHB08ylHPLMDzfunN2MpT02XrIp5UuEgkbIWoHVCgC77I7u80Uqm9o0vAmEUBcOE&usqp=CAU" alt="sql" />
 					<img class="border" src="https://miro.medium.com/max/870/1*l9IZ_LeUCP_vwxjDKI2Xgw.jpeg" alt="git" />
+					
 				</div>
 
 			</section>
